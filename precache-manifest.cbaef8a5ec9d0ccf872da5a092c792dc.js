9d16a4e76865f01540e095f5621830a86e508f05
JihyoH <rukaren@naver.com>
2020-04-28 23:12:38 +0900

    Updates

---
+++ precache-manifest.cbaef8a5ec9d0ccf872da5a092c792dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9531a1a6d6f5e07634f12f161f5c5c1a",
+    "url": "/react_board_practice/index.html"
+  },
+  {
+    "revision": "8437f4a2bdf013a0c1ea",
+    "url": "/react_board_practice/static/css/main.4388fadb.chunk.css"
+  },
+  {
+    "revision": "9eb5c759d942896a6285",
+    "url": "/react_board_practice/static/js/2.c3c34dec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_board_practice/static/js/2.c3c34dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8437f4a2bdf013a0c1ea",
+    "url": "/react_board_practice/static/js/main.cba3778b.chunk.js"
+  },
+  {
+    "revision": "8dad7b21965867ffb6b4",
+    "url": "/react_board_practice/static/js/runtime-main.a3b5301d.js"
+  },
+  {
+    "revision": "16a7a450cc2e8e7d4a6f3585970d729e",
+    "url": "/react_board_practice/static/media/stock-book1.16a7a450.jpg"
+  },
+  {
+    "revision": "d9291a32585a9e5c28e920fba3f16390",
+    "url": "/react_board_practice/static/media/stock-book2.d9291a32.jpg"
+  },
+  {
+    "revision": "37deb96df7fe1f0ba4842fd6bcbe7b55",
+    "url": "/react_board_practice/static/media/stock-book3.37deb96d.jpg"
+  }
+]);

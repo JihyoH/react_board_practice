639fa38113f262871bf87a8682d71eb6842fea65
JihyoH <rukaren@naver.com>
2020-05-03 12:05:36 +0900

    db forgive

--- src/components/Content4.js
+++ src/components/Content4.js
@@ -5,9 +5,11 @@ class Content4 extends Component{
   render(){
     return(
       <div>
-        <p>디비가 연결되었어요!</p>
+        <p>디비 연결</p>
         <div className="srcHierachy">
-            <pre className="codeView">444</pre>
+          <p>깃허브 블로그의 경우에는 static 페이지를 소스를 공개한 경우에 한해 무료로 호스팅해주고 있기 때문에,</p>
+          <p>firebase를 활용하면 디비 연결이 가능하다고 합니다...</p>
+          <p className="subDescription">언젠가는 해보는걸로!</p>
         </div>
       </div>
     )
